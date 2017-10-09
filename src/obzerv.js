49d93839876628abdc9d9d5263edcde8f6c4a989
callmecavs <callmecavs@gmail.com>
2017-10-09 16:07:54 -0400

    feature detection

--- src/obzerv.js
+++ src/obzerv.js
@@ -1,4 +1,8 @@
 const obzerv = () => {
+  // feature check
+  if (!'IntersectionObserver' in window) {
+    throw new Error('Intersection Observer is not supported in your browser: http://caniuse.com/#search=IntersectionObserver')
+  }
 
 }
 
